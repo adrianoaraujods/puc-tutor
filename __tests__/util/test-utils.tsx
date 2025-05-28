0e7e09b7a1a7dadcb94d243f2aa08a84aa8699d7
Adriano Araújo <adrianoaraujodsantos@hotmail.com>
2025-05-28 20:21:01 -0300

    build: initial commit

---
+++ __tests__/util/test-utils.tsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "next-themes";
+
+import type { RenderOptions } from "@testing-library/react";
+
+function TestProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      disableTransitionOnChange
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
+function customRender(
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) {
+  return render(ui, { wrapper: TestProviders, ...options });
+}
+
+export * from "@testing-library/react";
+export { customRender as render };
