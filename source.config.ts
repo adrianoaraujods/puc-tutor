d5e9d36cabd1abc8622b677af9489bfb7c2bf61a
Adriano Araújo <adrianoaraujodsantos@hotmail.com>
2025-06-19 18:43:21 -0300

    feat(content): add suport to mermaid graphs, latex and custom tabs

--- source.config.ts
+++ source.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, defineDocs } from "fumadocs-mdx/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 import { CONTENT_PATH } from "@/lib/config";
 
@@ -6,4 +8,10 @@ export const content = defineDocs({
   dir: CONTENT_PATH,
 });
 
-export default defineConfig({ lastModifiedTime: "git" });
+export default defineConfig({
+  lastModifiedTime: "git",
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+});
--- src/components/typography/mdx-components.tsx
+++ src/components/typography/mdx-components.tsx
@@ -1,10 +1,15 @@
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
+
+import { Mermaid } from "@/components/typography/mermaid";
 
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
+    Mermaid,
     ...components,
   };
 }
